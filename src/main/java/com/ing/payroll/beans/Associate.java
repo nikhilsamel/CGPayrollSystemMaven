63894340bce57d866ceea42b3d235cbdf4672569
nsamel <nsamel@LIN51005457.corp.capgemini.com>
2017-11-30 14:42:18 +0530

    first commit

--- src/main/java/com/ing/payroll/beans/Associate.java
+++ src/main/java/com/ing/payroll/beans/Associate.java
@@ -1,7 +1,6 @@
 package com.ing.payroll.beans;
 
 public class Associate {
-	//abcd
 	private int associateID,yearlyInvestmentUnder80C;
 	private String firstName,lastName,department,designation,pancard,emailId;
 	private Salary salary;
